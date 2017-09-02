c0cd75f8c1d185fbbcbfbcb3e1dd223b50a15f98
Noxyra <noxyra@gmail.com>
2017-09-02 13:19:19 +0200

    Entité personnage (mode jeux de combat)

---
+++ Entitees/Personnage.php
@@ -0,0 +1,80 @@
+<?php
+    class Personnage{
+        // Variables
+        private $_id;
+        private $_degats;
+        private $_nom;
+
+        // Constantes
+        const CEST_MOI = 1;
+        const PERSONNAGE_TUE = 2;
+        const PERSONNAGE_FRAPPE = 3;
+
+        // Getters
+        public function getId(){
+            return $this->_id;
+        }
+
+        public function getDegats(){
+            return $this->_degats;
+        }
+
+        public function getNom(){
+            return $this->_nom;
+        }
+
+        // Setters
+        public function setId($id){
+            $id = (int) $id;
+            $this->_id = $id;
+        }
+
+        public function setDegats($degats){
+            $degats = (int) $degats;
+            if ($degats >= 0 && $degats <= 100){
+                $this->_degats = $degats;
+            }
+        }
+
+        public function setNom($nom){
+            if(is_string($nom)){
+                $this->_nom = $nom;
+            }
+        }
+
+        // Construction
+        public function __construct(array $donnees)
+        {
+            $this->hydrate($donnees);
+        }
+
+        // Hydratation
+        public function hydrate(array $donnees){
+            foreach ($donnees as $k => $v){
+                $method = "set" . ucfirst($k);
+                if(method_exists($this, $method)){
+                    $this->$method($v);
+                }
+            }
+        }
+
+        // Methode d'actions
+
+        public function frapper(Personnage $personnage){
+            if($this->getId() === $personnage->getId()){
+                self::CEST_MOI;
+            }
+
+            $personnage->recevoirDegats();
+        }
+
+        public function recevoirDegats(){
+            $this->_degats += 5;
+
+            if($this->_degats >= 100){
+                return self::PERSONNAGE_TUE;
+            }
+
+            return self::PERSONNAGE_TUE;
+        }
+    }
