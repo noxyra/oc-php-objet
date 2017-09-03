5bf310e31c663d76143dce0a10e3387a9907ce1e
Noxyra <noxyra@gmail.com>
2017-09-03 15:06:32 +0200

    Exception : Test withCatch

--- Exeptions/firstTest.php
+++ Exeptions/firstTest.php
@@ -10,6 +10,14 @@
         return $a + $b;
     }
 
-    echo additionner(12, 13) . '<br />';
-    echo additionner("azerty", 54);
-    echo additionner(4,8);
+    try{
+        echo additionner(12, 13) . '<br />';
+        echo additionner("azerty", 54);
+        echo additionner(4,8);
+    }
+    catch (Exception $e)
+    {
+        echo "Une exception a été lancé : " . $e->getMessage();
+    }
+
+    echo "<br />Fin du script.";
